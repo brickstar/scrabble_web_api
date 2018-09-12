0991b9e9f87869f79ea9781fe35d856b772880f4
brickstar <brickstar@gmail.com>
2018-09-12 09:45:15 -0600

    struggling with header parameters

--- app/presenters/presenter.rb
+++ app/presenters/presenter.rb
@@ -1,10 +1,26 @@
 class Presenter
-  attr_reader :word
+  attr_reader :word, :conn
   def initialize(params)
     @word = params[:search]
   end
 
   def root_word
     binding.pry
+
+  end
+
+
+  def conn
+    @conn ||= Faraday.new(url: "https://od-api.oxforddictionaries.com/api/v1")
+  end
+
+  def response
+    conn =  Faraday.new(url: "https://od-api.oxforddictionaries.com/api/v1")
+    conn.get |req| do
+      req.url("inflections/en/#{word}")
+      req.headers['Content-Type'] = 'application/json'
+      req.headers['app_id'] = 'cb7a6cb9'
+      req.headers['app_key'] = '30b83590a6facfe5506d0f7619293dc5'
+    end
   end
 end
